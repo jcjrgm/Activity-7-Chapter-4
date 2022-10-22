510b76da5ba79c5556f83c949633d1a209a56b28
jcjrgm <108171568+jcjrgm@users.noreply.github.com>
2022-10-21 23:25:42 -0700

    Add files via upload

--- sales_tax.js
+++ sales_tax.js
@@ -2,26 +2,39 @@
 
 const $ = selector => document.querySelector(selector);
 
-function processEntries() {
-	var subtotal = document.getElementById('subtotal').value;
-	var tax_rate = document.getElementById('tax_rate').value;
-	if (parseFloat(subtotal) < 0 || parseFloat(subtotal) > 10000) {
-		alert("Enter subtotal value between 1 - 10000");
+var processEntries=function(){
+	if($('subtotal').value<10000 && $('subtotal').value>0 && $('tax_rate').value>0 && $('tax_rate').value<12 ) {
+		$('sales_tax').value = ($('subtotal').value*$('tax_rate').value)/100;
+		$('total').value = parseFloat($('sales_tax').value)+parseFloat($('subtotal').value);
+		$('subtotal').focus();
 	}
-	else if (parseFloat(tax_rate) < 0 || parseFloat(tax_rate) > 10000) {
-		alert("Enter tax rate value between 1 - 12");
-	}
+
 	else {
-		var tax = (parseFloat(subtotal) * parseFloat(tax_rate)) / 100;
-		var total = parseFloat(tax) + parseFloat(subtotal);
-		document.getElementById("sales_tax").value = tax.toFixed(2);
-		document.getElementById("total").value = total.toFixed(2);
+		alert("Subtotal must be >0 and <10000 \n Tax rate must be >0 and <12")
 	}
+
 }
 
-function clearForm() {
-	document.getElementById('subtotal').value = "";
-	document.getElementById('tax_rate').value = "";
-	document.getElementById('sales_tax').value = "";
-	document.getElementById('total').value = "";
+var clearEntries=function(){
+$('sales_tax').value='';
+$('total').value='';
+$('subtotal').value='';
+$('tax_rate').value='';
+$('subtotal').focus();
 }
+
+var clearSubtotal=function(){
+$('subtotal').value='';
+}
+
+var clearTaxRate=function(){
+$('tax_rate').value='';
+}
+
+window.onload=function(){
+$('subtotal').focus();
+$('calculate').onclick=processEntries;
+$('clear').onclick=clearEntries;
+$('subtotal').onclick=clearSubtotal;
+$('tax_rate').onclick=clearTaxRate;
+};
